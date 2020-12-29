42fa8bf4234f004fe78f70bf626e00f78ec230ca
Annisa <annisa@gigaming.io>
2020-12-29 11:41:55 +0700

    add package and initialization webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: 'style-loader',
+                },
+                {
+                    loader: 'css-loader',
+                },
+            ],
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif)$/,
+            use: [
+                'file-loader',
+            ],
+        },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'src/index.html'),
+  }),
+//   new CopyWebpackPlugin({
+//     patterns: [
+//         {
+//             from: path.resolve(__dirname, 'src/images'),
+//             to: path.resolve(__dirname, 'dist/images'),
+//         },
+//       ],
+//   }),
+  new CleanWebpackPlugin()
+  ]
+}
